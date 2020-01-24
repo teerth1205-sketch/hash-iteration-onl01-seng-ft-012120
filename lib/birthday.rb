1c301780c3f1088a17bad6da7dc748cbe09ff7da
Teerth Patel <teerth1205@gmail.com>
2020-01-24 21:06:21 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,7 +14,7 @@ def age_appropriate_birthday(birthday_kids)
    birthday_kids.each do |kids_name, age|
     if age < 12  
       puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    else
+    elsif age > 12
       "You are too old for this."
     end
   end 
